9a0403a28260a0963d954ca60437de277d40e538
Deokma <dpopolamovmail@gmail.com>
2023-02-11 19:12:36 +0300

    update to version 1.6

---
+++ src/main/resources/static/js/themes.js
@@ -0,0 +1,40 @@
+// const button = document.getElementById('toggle-button');
+// const div = document.querySelector('div');
+//
+// button.addEventListener('click', function() {
+//     if (div.classList.contains('light-theme')) {
+//         console.log("light")
+//         div.classList.remove('light-theme');
+//         div.classList.add('dark-theme');
+//
+//     } else {
+//         console.log("dark")
+//         div.classList.remove('dark-theme');
+//         div.classList.add('light-theme');
+//
+//     }
+//     console.log("213124")
+// });
+
+const toggleButton = document.getElementById('toggle-button');
+const div = document.querySelector('div');
+
+toggleButton.addEventListener('click', function () {
+    if (div.classList.contains('light-theme')) {
+        console.log("light")
+        div.classList.remove('light-theme');
+        div.classList.add('dark-theme');
+        localStorage.setItem('theme', 'dark-theme');
+    } else {
+        console.log("dark")
+        div.classList.remove('dark-theme');
+        div.classList.add('light-theme');
+        localStorage.setItem('theme', 'light-theme');
+    }
+});
+
+const element = document.getElementById('element');
+const storedTheme = localStorage.getItem('theme');
+if (storedTheme) {
+    div.classList.add(storedTheme);
+}
